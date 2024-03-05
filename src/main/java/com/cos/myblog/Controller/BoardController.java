f27b658bfd540ef5956a8b328740e75b1244adb5
turtle <lsiwh32749@gmail.com>
2024-03-05 19:10:35 +0900

    글 상세보기

--- src/main/java/com/cos/myblog/Controller/BoardController.java
+++ src/main/java/com/cos/myblog/Controller/BoardController.java
@@ -1,5 +1,6 @@
 package com.cos.myblog.Controller;
 
+import com.cos.myblog.Repository.BoardRepository;
 import com.cos.myblog.Service.BoardService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Pageable;
@@ -8,18 +9,24 @@ import org.springframework.data.web.PageableDefault;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 @Controller
 @RequiredArgsConstructor
 public class BoardController {
 
     private final BoardService boardService;
-
     //@AuthenticationPrincipal PrincipalDetail principal
     @GetMapping({"","/"})
     public String index(Model model, @PageableDefault(size=3, sort="id", direction = Sort.Direction.DESC)Pageable pageable){
         model.addAttribute("boards", boardService.글목록(pageable));
         return "index";
+    }
+    @GetMapping("/board/{id}")
+    public String findById(@PathVariable int id, Model model){
+        model.addAttribute("board", boardService.글상세보기(id));
+        boardService.글상세보기(id);
+        return "board/detail";
     }
 
     @GetMapping("/board/saveForm")
